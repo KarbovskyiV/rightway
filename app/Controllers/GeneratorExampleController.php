cbacaf8b1474d572439ca21d105f71bec5f87817
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-08-01 13:22:39 +0300

    use route with attributes

--- app/Controllers/GeneratorExampleController.php
+++ app/Controllers/GeneratorExampleController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Attributes\Route;
 use App\Models\Ticket;
 
 class GeneratorExampleController
@@ -12,7 +13,8 @@ class GeneratorExampleController
     {
     }
 
-    public function index()
+    #[Route('/examples/generator')]
+    public function index(): void
     {
         foreach ($this->ticketModel->all() as $ticket) {
             echo $ticket['id'] . ': ' . substr($ticket['email'], 0, 15) . '<br/>';
