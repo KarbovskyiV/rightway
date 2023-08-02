cc6494efb4166757c1d724d830d1717f6532ed7d
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-08-02 14:13:34 +0300

    creates example for using doctrine dbal

---
+++ app/doctrine_dbal_example.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+use Doctrine\DBAL\DriverManager;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$connectionParams = [
+    'dbname' => $_ENV['DB_DATABASE'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASS'],
+    'host' => $_ENV['DB_HOST'],
+    'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
+];
+$conn = DriverManager::getConnection($connectionParams);
+
+$schema = $conn->createSchemaManager();
+
+var_dump($schema->listTableNames());
