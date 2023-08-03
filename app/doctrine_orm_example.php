93635b74d440412af3d746b5d110246adaa0a7a9
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-08-03 11:42:54 +0300

    clean example

--- app/doctrine_orm_example.php
+++ app/doctrine_orm_example.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Entity\Invoice;
-use App\Entity\InvoiceItem;
-use App\Enums\InvoiceStatus;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 use Dotenv\Dotenv;
@@ -22,35 +19,10 @@ $params = [
     'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
 ];
 
-$entityManager = EntityManager::create($params, Setup::createAttributeMetadataConfiguration([__DIR__ . '/Entity']));
+$entityManager = EntityManager::create(
+    $params,
+    Setup::createAttributeMetadataConfiguration([__DIR__ . '/Entity'])
+);
 
-$items = [['item 1', 1, 15], ['Item 2', 2, 7.5], ['Item 3', 4, 3.75]];
+$queryBuilder = $entityManager->createQueryBuilder();
 
-//$invoice = (new Invoice())
-//    ->setAmount(45)
-//    ->setInvoiceNumber("1")
-//    ->setStatus(InvoiceStatus::Pending)
-//    ->setCreatedAt(new DateTime());
-//
-//foreach ($items as [$description, $quantity, $unitPrice]) {
-//    $item = (new InvoiceItem())
-//        ->setDescription($description)
-//        ->setQuantity($quantity)
-//        ->setUnitPrice($unitPrice);
-//
-//    $invoice->addItem($item);
-//}
-
-//$entityManager->persist($invoice);
-
-$invoice = $entityManager->find(Invoice::class, 51);
-
-$invoice->setStatus(InvoiceStatus::Paid);
-$invoice->getItems()->get(0)->setDescription('Test');
-
-$entityManager->flush();
-
-//$entityManager->remove($invoice);
-//$entityManager->flush();
-
-//echo $entityManager->getUnitOfWork()->size();
