f46ee7432cb9434a27d649b13d57ff6cc2afe234
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-08-08 10:52:08 +0300

    example of using VO

---
+++ app/shipping_calculator_example.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Services\Shipping\BillableWeightCalculatorService;
+use App\Services\Shipping\DimDivisor;
+use App\Services\Shipping\PackageDimension;
+use App\Services\Shipping\Weight;
+
+$package = [
+    'weight' => 6,
+    'dimension' => [
+        'width' => 9,
+        'length' => 15,
+        'height' => 7,
+    ],
+];
+
+$packageDimension = new PackageDimension(
+    $package['dimension']['width'],
+    $package['dimension']['height'],
+    $package['dimension']['length'],
+);
+
+$weight = new Weight($package['weight']);
+$billableWeightService = new BillableWeightCalculatorService();
+
+$widerPackageDimension = $packageDimension->increaseWidth(10);
+
+$billableWeight = $billableWeightService->calculate(
+    $packageDimension,
+    $weight,
+    DimDivisor::FEDEX
+);
+
+$widerPackageBillableWeight = $billableWeightService->calculate(
+    $widerPackageDimension,
+    $weight,
+    DimDivisor::FEDEX
+);
+
+echo $billableWeight . ' lb' . PHP_EOL;
+echo $widerPackageBillableWeight . ' lb' . PHP_EOL;
