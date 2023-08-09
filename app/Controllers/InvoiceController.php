bb5e48e00ab0481c7371937c23cc191efc54ae9f
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-08-09 09:15:58 +0300

    delete example, adds map to query

--- app/Controllers/InvoiceController.php
+++ app/Controllers/InvoiceController.php
@@ -8,30 +8,25 @@ use App\Attributes\Get;
 use App\Enums\InvoiceStatus;
 use App\Models\Invoice;
 use App\View;
-use Carbon\Carbon;
 
 class InvoiceController
 {
     #[Get('/invoices')]
     public function index(): View
     {
-        $invoices = Invoice::query()->where('status', InvoiceStatus::Paid)->get()->toArray();
+        $invoices = Invoice::query()
+            ->where('status', InvoiceStatus::Paid)
+            ->get()
+            ->map(
+                fn(Invoice $invoice) => [
+                    'invoiceNumber' => $invoice->invoice_number,
+                    'amount' => $invoice->amount,
+                    'status' => $invoice->status->toString(),
+                    'dueDate' => $invoice->due_date->toDateTimeString(),
+                ]
+            )
+            ->toArray();
 
         return View::make('invoices/index', ['invoices' => $invoices]);
-    }
-
-    #[Get('/invoices/new')]
-    public function create()
-    {
-        $invoice = new Invoice();
-
-        $invoice->invoice_number = 5;
-        $invoice->amount = 20;
-        $invoice->status = InvoiceStatus::Pending;
-        $invoice->due_date = (new Carbon())->addDay();
-
-        $invoice->save();
-
-        echo $invoice->id . ', ' . $invoice->due_date->format('m/d/Y');
     }
 }
