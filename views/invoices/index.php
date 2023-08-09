80634e82dd8e726486891af0cb14846792b2554f
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-08-09 09:16:21 +0300

    adds if statements

--- views/invoices/index.php
+++ views/invoices/index.php
@@ -1,3 +1,31 @@
+<style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        text-align: center;
+    }
+
+    table tr th, table tr td {
+        border: 1px #eee solid;
+        padding: 5px;
+    }
+
+    .color-green {
+        color: green;
+    }
+
+    .color-red {
+        color: red;
+    }
+
+    .color-gray {
+        color: gray;
+    }
+
+    .color-orange {
+        color: orange;
+    }
+</style>
 <table>
     <thead>
     <tr>
@@ -8,25 +36,33 @@
     </tr>
     </thead>
     <tbody>
-    <?php if (empty($invoices)): ?>
+    <?php
+    if (empty($invoices)): ?>
         <tr>
             <td colspan="4">No Invoices Found</td>
         </tr>
-    <?php else: ?>
-        <?php foreach ($invoices as $invoice): ?>
+    <?php
+    else: ?>
+        <?php
+        foreach ($invoices as $invoice): ?>
             <tr>
                 <td><?= $invoice['invoiceNumber'] ?></td>
                 <td>$<?= number_format($invoice['amount'], 2) ?></td>
                 <td><?= $invoice['status'] ?></td>
                 <td>
-                    <?php if ($invoice['dueDate']): ?>
+                    <?php
+                    if ($invoice['dueDate']): ?>
                         <?= date('m/d/Y', strtotime($invoice['dueDate'])) ?>
-                    <?php else: ?>
+                    <?php
+                    else: ?>
                         N/A
-                    <?php endif ?>
+                    <?php
+                    endif ?>
                 </td>
             </tr>
-        <?php endforeach; ?>
-    <?php endif ?>
+        <?php
+        endforeach; ?>
+    <?php
+    endif ?>
     </tbody>
 </table>
