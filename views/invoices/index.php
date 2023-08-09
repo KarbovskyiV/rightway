3f2fbe80100c448d2baf7242d9a14ee927e2205f
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-08-09 09:14:30 +0300

    creates template blade example

--- views/invoices/index.php
+++ views/invoices/index.php
@@ -1,47 +1,32 @@
-<style>
-    table {
-        width: 100%;
-        border-collapse: collapse;
-        text-align: center;
-    }
-
-    table tr th, table tr td {
-        border: 1px #eee solid;
-        padding: 5px;
-    }
-
-    .color-green {
-        color: green;
-    }
-
-    .color-red {
-        color: red;
-    }
-
-    .color-gray {
-        color: gray;
-    }
-
-    .color-orange {
-        color: orange;
-    }
-</style>
 <table>
     <thead>
     <tr>
-        <th>Invoice #</th>
+        <th>Invoice Number</th>
         <th>Amount</th>
         <th>Status</th>
+        <th>Due Date</th>
     </tr>
     </thead>
     <tbody>
-    <?php foreach ($invoices as $invoice): ?>
+    <?php if (empty($invoices)): ?>
         <tr>
-            <td><?= $invoice['invoice_number'] ?></td>
-            <td>$<?= number_format($invoice['amount'], 2) ?></td>
-            <td class="<?= \App\Enums\InvoiceStatus::tryFrom($invoice['status'])->color()->getClass() ?>">
-                <?= \App\Enums\InvoiceStatus::tryFrom($invoice['status'])->toString() ?></td>
+            <td colspan="4">No Invoices Found</td>
         </tr>
-    <?php endforeach; ?>
+    <?php else: ?>
+        <?php foreach ($invoices as $invoice): ?>
+            <tr>
+                <td><?= $invoice['invoiceNumber'] ?></td>
+                <td>$<?= number_format($invoice['amount'], 2) ?></td>
+                <td><?= $invoice['status'] ?></td>
+                <td>
+                    <?php if ($invoice['dueDate']): ?>
+                        <?= date('m/d/Y', strtotime($invoice['dueDate'])) ?>
+                    <?php else: ?>
+                        N/A
+                    <?php endif ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif ?>
     </tbody>
 </table>
