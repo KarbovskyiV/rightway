d5601a885b8f70bf597074dbb5adbcc5abe764d4
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-08-09 13:31:14 +0300

    removes email and apiKeys

--- app/Config.php
+++ app/Config.php
@@ -6,8 +6,6 @@ namespace App;
 
 /**
  * @property-read ?array $db
- * @property-read ?array $mailer
- * @property-read ?array $apiKeys
  */
 class Config
 {
@@ -18,21 +16,11 @@ class Config
         $this->config = [
             'db' => [
                 'host' => $env['DB_HOST'],
-                'username' => $env['DB_USER'],
+                'user' => $env['DB_USER'],
                 'password' => $env['DB_PASS'],
-                'database' => $env['DB_DATABASE'],
-                'driver' => $env['DB_DRIVER'] ?? 'mysql',
-                'charset' => 'utf8',
-                'collation' => 'utf8_unicode_ci',
-                'prefix' => '',
+                'dbname' => $env['DB_DATABASE'],
+                'driver' => $env['DB_DRIVER'] ?? 'pdo_mysql',
             ],
-            'mailer' => [
-                'dsn' => $env['MAILER_DSN'] ?? '',
-            ],
-            'apiKeys' => [
-                'emailable' => $_ENV['EMAILABLE_API_KEY'] ?? '',
-                'abstract_api_email_validation' => $_ENV['ABSTRACT_API_EMAIL_VALIDATION_API_KEY'] ?? '',
-            ]
         ];
     }
 
